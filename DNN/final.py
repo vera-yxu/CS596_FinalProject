417dfc7f53bca0ea8dc1c3df3a8a71582e20260c
basilinb <36709567+basilinb@users.noreply.github.com>
2018-12-21 18:57:05 -0800

    Add files via upload

--- DNN/final.py
+++ DNN/final.py
@@ -1,20 +1,20 @@
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
 
+# Creating function to calculate accuracy from confusion matrix
+def accuracy(confusion_matrix):
+    diagonal_sum = confusion_matrix.trace()
+    sum_of_all_elements = confusion_matrix.sum()
+    return (diagonal_sum / sum_of_all_elements)*100
+# Data Pre-processing
 with open('splice.csv', 'r') as infile:
     data = infile.readlines()
     data = [i.replace('\n', ' ').replace(' ', '').split(',') for i in data]
@@ -46,19 +46,19 @@ with open('splice.csv', 'r') as infile:
     # Base pairs split into separate columns
     df2.to_csv("data.csv", encoding='utf-8', index=False)
 
-
-
 dataset = pd.read_csv('data.csv')
 dataset["cat"]=dataset["cat"].astype('category')
+np.random.seed(101)
+dataset = dataset.sample(frac=1).reset_index(drop=True)
 i = pd.DataFrame(dataset)
 seq = i.values
 
+# Making Training and Test datasets
+X = seq[0:, 0:60].astype(float) # features
+Y = seq[0:, 60] # labels
 
-X = seq[0:, 0:60].astype(float)
-Y = seq[0:, 60]
 # Set Seed
-seed = 7
-np.random.seed(seed)
+np.random.seed(7)
 
 # Label encode Class
 encoder = LabelEncoder()
@@ -66,21 +66,49 @@ encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
 # One Hot Encode
 y_dummy = np_utils.to_categorical(encoded_Y)
+X_train,X_test, y_train,y_test = train_test_split(X,y_dummy,test_size=0.2,random_state=0)
 
-# Deep Learnig Function
-def deepml_model():
-    # Model Creation
-    deepml = Sequential()
-    deepml.add(Dense(8, input_dim=60, activation='relu'))
-    deepml.add(Dense(3, activation='softmax')) # Using softmax to ensure that the output values are in the range of 0 to 1 so that it can be used as predicted probabilities
-    # Model Compilation
-    #Here we are using the Adam gradient descent optimization with a logarithimic loss function called categorical_crossentropy
-    deepml.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
-    return deepml
+# Model Creation
+deepml = Sequential()
+deepml.add(Dense(10, input_dim=60, activation='relu'))
+deepml.add(Dense(8,activation='relu'))
+deepml.add(Dense(5,activation='relu'))
+deepml.add(Dense(3, activation='softmax'))
+# Model Compilation
+deepml.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+deepml.summary()
 
-estimate = KerasClassifier(build_fn=deepml_model, epochs=20, batch_size=5, verbose=0)
+#fitting the model and predicting
+history = deepml.fit(X_train, y_train, epochs=200 ,validation_data=(X_test, y_test))
+y_pred = deepml.predict(X_test)
 
-k_fold = KFold(n_splits=10, shuffle=True, random_state=seed) #K-fold crossvalidation is used here with 10 splits
+y_test_class = np.argmax(y_test,axis=1) # convert encoded labels into classes
+y_pred_class = np.argmax(y_pred,axis=1) # convert predicted labels into classes
+#Accuracy of the predicted values
+print(classification_report(y_test_class, y_pred_class)) # Precision , Recall, F1-Score & Support
+cm = confusion_matrix(y_test_class, y_pred_class)
+print(cm)
+print("Model Accuracy: {} %" .format(accuracy(cm)))
 
-results = cross_val_score(estimate, X, y_dummy, cv=k_fold)
-print("Model: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+training_loss = history.history['loss']
+test_loss = history.history['val_loss']
+
+# Create count of the number of epochs
+epoch_count = range(1, len(training_loss) + 1)
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# Visualize loss history
+plt.plot(epoch_count, training_loss, 'r--')
+plt.plot(epoch_count, test_loss, 'b-')
+plt.legend(['Training Loss', 'Test Loss'])
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show();
+
+
